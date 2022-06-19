15203eb41b1270e04054cbbe4ae26d70a2ffe366
Jenipher Belloso <jenipherbelloso@gmail.com>
2022-06-18 21:34:44 -0600

    commit tree c50774fdb3a8e9d4fc40c7f2b17372b1c03090af

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const messages = []
+function writeCards(name, event){
+    for (let i = 0; i <name.length; i++){
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+function countDown (){
+    
+    var countdown = 10;{
+        while (countdown >= 0){
+            console.log(countdown);
+            countdown = countdown - 1;
+        }
+        
+    }
+}
